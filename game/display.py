9c2a7fbc05334b22591ac5a793b169e622f38019
kokos Rybnik <vojtabern@gmail.com>
2022-01-02 15:08:51 +0100

    movement

--- game/display.py
+++ game/display.py
@@ -12,16 +12,17 @@ class Display:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.step = 5
     def window(self):
         pygame.init()
         display = pygame.display.set_mode((500, 400))
         player_texture = pygame.image.load("toast.png")
         pygame.display.set_caption("Game")
-        texture = display.blit(player_texture, (self.x, self.y, 50, 50))
-
-        fps = 30
         fpsclock = pygame.time.Clock()
         while True:
+            fpsclock.tick(60)
+            display.fill([0,0,0])
+            keys = pygame.key.get_pressed()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
@@ -29,12 +30,20 @@ class Display:
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                         return
-            #difference = movement.Movement(self.x, self.y).move()
-            texture = display.blit(player_texture, (self.x, self.y, 50, 50))
-            pygame.display.flip()
-            # window.fill(0)
-            pygame.display.update()
-            fpsclock.tick(fps)
+
+                elif keys:
+                    if keys[pygame.K_w]:
+                        self.y -= self.step
+                    if keys[pygame.K_s]:
+                        self.y += self.step
+                    if keys[pygame.K_d]:
+                        self.x += self.step
+                    if keys[pygame.K_a]:
+                        self.x -= self.step
+
+                display.blit(player_texture, (self.x, self.y, 50, 50))
+                pygame.display.flip()
+                pygame.display.update()
 
 
 Display(250,250).window()
