3ea10c92b3cd91e123600bdbf825f42f156e07cc
vojtabern <vojtabern@gmail.com>
2022-01-13 11:17:16 +0100

    working_on

--- game/display.py
+++ game/display.py
@@ -7,6 +7,7 @@ import random
 import change_tuple
 import movement
 import spawn
+import pygame.freetype
 
 
 
@@ -17,6 +18,7 @@ class Display:
         self.step = 5
         self.max_x = 1000
         self.max_y = 500
+        self.score = 0
     def window(self):
         pygame.init()
         display = pygame.display.set_mode((self.max_x, self.max_y))
@@ -28,7 +30,6 @@ class Display:
         apple_y = random.randrange(99, display.get_height())
         pygame.draw.rect(display, [255, 0, 0], pygame.Rect(apple_x, apple_y, 5, 5))
         while True:
-
             fpsclock.tick(60)
             display.fill([0,0,0])
             keys = pygame.key.get_pressed()
@@ -52,13 +53,14 @@ class Display:
                 self.x = movement.Movement(self.max_x, draw_player, self.x, "x").move()
                 self.y = movement.Movement(self.max_y, draw_player, self.y, "y").move()
                 draw_player = display.blit(player_texture, (self.x, self.y, 50, 50))
-
+                print (f"{self.score}")
 
                 pygame.display.flip()
                 apple = pygame.draw.rect(display, [255, 0, 0], pygame.Rect(apple_x, apple_y, 50, 50))
                 if draw_player.colliderect(apple):
-                    apple_x = random.randrange(99, display.get_width())
-                    apple_y = random.randrange(99, display.get_height())
+                    self.score += 1
+                    apple_x = random.randrange(107, display.get_width())
+                    apple_y = random.randrange(107, display.get_height())
                     apple = pygame.draw.rect(display, [255, 0, 0], pygame.Rect(apple_x, apple_y, 50, 50))
                 pygame.display.update()
 
