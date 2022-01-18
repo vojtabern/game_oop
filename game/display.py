4557dfb4616797ca37d060b60ef53b2ea96d82a0
kokos Rybnik <vojtabern@gmail.com>
2022-01-18 19:41:59 +0100

    Snake will be later, Now working eating game

--- game/display.py
+++ game/display.py
@@ -1,14 +1,11 @@
 
-
 import keyword
-
 import pygame
 import sys
 import time
 import random
 import change_tuple
 import movement
-import body
 import pygame.freetype
 
 
@@ -22,18 +19,23 @@ class Display:
         self.max_y = 500
         self.score = 0
         self.vector = 'd'
+        self.width = 107
+        self.height = 99
+
+
     def window(self):
         pygame.init()
         display = pygame.display.set_mode((self.max_x, self.max_y))
         player_texture= pygame.image.load("toast.png")
         pygame.display.set_caption("Game")
         fpsclock = pygame.time.Clock()
-        #draw_player = display.blit(player_texture, (self.x, self.y, 50, 50))
+        draw_player = display.blit(player_texture, (self.x, self.y, 50, 50))
         apple_x = random.randrange(99, self.max_x-50)
         apple_y = random.randrange(99, self.max_y-50)
         font = pygame.font.Font('freesansbold.ttf', 32)
-
-        while True:
+        running = True
+        starting_position = (self.x, self.y)
+        while running:
             fpsclock.tick(60)
             display.fill([0,0,0])
             keys = pygame.key.get_pressed()
@@ -42,8 +44,8 @@ class Display:
                 if event.type == pygame.QUIT:
                     return
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE or movement.Movement(self.max_y, draw_player, self.y, "y").control_y(self.vector)==False\
-                            or movement.Movement(self.max_x, draw_player, self.x, "x").control_x(self.vector)==False:
+                    if event.key == pygame.K_ESCAPE or movement.Movement(self.max_y, draw_player, self.y, "y", self.width, self.height).control_y(self.vector)==False\
+                            or movement.Movement(self.max_x, draw_player, self.x, "x", self.width, self.height).control_x(self.vector)==False:
                         print(f"you have achieved {self.score} score. ")
                         pygame.quit()
                         return
@@ -91,11 +93,12 @@ class Display:
                 self.y = self.y + 0
                 self.x = self.x - 1
 
-            # self.x = movement.Movement(self.max_x, draw_player, self.x, "x").move()
-            # self.y = movement.Movement(self.max_y, draw_player, self.y, "y").move()
-            draw_player = display.blit(player_texture, [self.x, self.y, 50, 50])
-            print(f"{self.score}")
 
+            self.x = movement.Movement(self.max_x, draw_player, self.x, "x", self.width, self.height).move()
+            self.y = movement.Movement(self.max_y, draw_player, self.y, "y", self.width, self.height).move()
+            #draw_player = display.blit(player_texture, [self.x, self.y, 50, 50])
+            draw_player = pygame.draw.rect(display, [255, 225, 0], pygame.Rect(self.x, self.y, self.width, self.height))
+            print(f"{self.score}")
 
             # score code
             text = font.render(f'Score: {self.score}', True, [255, 0, 0])
@@ -103,14 +106,20 @@ class Display:
             display.blit(text, textRect)
             pygame.display.flip()
                 # end of score code
-
                 # start of spawn code
             apple = pygame.draw.rect(display, [255, 0, 0], pygame.Rect(apple_x, apple_y, 50, 50))
             if draw_player.colliderect(apple):
                 self.score += 1
+                if self.max_y > self.height and self.max_x > self.width:
+                    self.height += 5
+                    self.width += 5
+                elif self.max_y <= self.height or self.max_x <= self.width:
+                    print("Vyhral jsi")
+
                 apple_x = random.randrange(107, self.max_x-50)
                 apple_y = random.randrange(107, self.max_y-50)
                 apple = pygame.draw.rect(display, [255, 0, 0], pygame.Rect(apple_x, apple_y, 50, 50))
+
 
             pygame.display.update()
 
--- game/movement.py
+++ game/movement.py
@@ -4,37 +4,40 @@ import change_tuple
 
 
 class Movement():
-    def __init__(self,display, player, xy, axis):
+    def __init__(self,display, player, xy, axis, width, height):
         self.xy = xy
         self.display = display
         self.player = player
         self.step = 5
+        self.width = width
+        self.height = height
         self.axis = axis
 
-    # def move(self):
-    #     if self.axis == "x":
-    #         if self.xy + 107 >= self.display:
-    #             self.xy = self.xy - self.step
-    #
-    #             #change_tuple.Vector2[0].__add__(-5)
-    #             return self.xy
-    #         elif self.xy <= 0:
-    #             self.xy = self.xy + self.step
-    #             #change_tuple.Vector2[0].__add__(5)
-    #             return self.xy
-    #     elif self.axis == "y":
-    #         if self.xy+99 >= self.display:
-    #             self.xy = self.xy - self.step
-    #             #change_tuple.Vector2[1].__add__(-5)
-    #             return self.xy
-    #         elif self.xy - 5 <= 0:
-    #             self.xy = self.xy + self.step
-    #             return self.xy
-    #     return self.xy
+
+    def move(self):
+        if self.axis == "x":
+            if self.xy + self.width >= self.display:
+                self.xy = self.xy - self.step
+
+                #change_tuple.Vector2[0].__add__(-5)
+                return self.xy
+            elif self.xy <= 0:
+                self.xy = self.xy + self.step
+                #change_tuple.Vector2[0].__add__(5)
+                return self.xy
+        elif self.axis == "y":
+            if self.xy+self.height >= self.display:
+                self.xy = self.xy - self.step
+                #change_tuple.Vector2[1].__add__(-5)
+                return self.xy
+            elif self.xy - 5 <= 0:
+                self.xy = self.xy + self.step
+                return self.xy
+        return self.xy
 
     def control_x(self, vector):
         if self.axis == "x":
-            if self.xy + 111 >= self.display and vector == 'd':
+            if self.xy + self.width + 4 >= self.display and vector == 'd':
                 return False
             elif self.xy - 4 <= 0 and vector == 'a':
                 return False
@@ -42,7 +45,7 @@ class Movement():
 
     def control_y(self, vector):
         if self.axis == "y":
-            if self.xy + 103 > self.display and vector == 's':
+            if self.xy + self.height + 4 > self.display and vector == 's':
                 return False
             elif self.xy -19 <= 0 and vector == 'w':
                 return False
